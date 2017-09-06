d73afca65ad2e239150ce905f4289feba7ed0267
Scott Baker <smbaker@gmail.com>
2017-09-05 22:35:25 -0700

    CORD-1762 implement AddressManager model policies
    Change-Id: Iaab6c84e8d94240b541193e6b5786b184cff683b
    (cherry picked from commit 353a2f0d30bd4c75474f62da22861832ad55ab87)

---
+++ xos/synchronizer/model_policies/model_policy_addressmanagerserviceinstance.py
@@ -0,0 +1,31 @@
+
+# Copyright 2017-present Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from synchronizers.new_base.modelaccessor import *
+from synchronizers.new_base.policy import Policy
+
+class AddressManagerServiceInstancePolicy(Policy):
+    model_name = "AddressManagerServiceInstance"
+
+    def handle_create(self, service_instance):
+        return self.handle_update(service_instance)
+
+    def handle_update(self, service_instance):
+        if (service_instance.link_deleted_count>0) and (not service_instance.provided_links.exists()):
+            # if the last provided_link has just gone away, then self-destruct
+            self.logger.info("The last provided link has been deleted -- self-destructing.");
+            service_instance.delete()
+            return
